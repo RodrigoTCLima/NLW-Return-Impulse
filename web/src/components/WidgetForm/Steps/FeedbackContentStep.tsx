3eeeb5e881b49fcb7ce5a8f2fce10c30e2ca4c06
Rodrigo Lima <rodrigotiagocl@gmail.com>
2022-05-05 19:12:41 -0300

    Término da parte pós envio

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -6,11 +6,13 @@ import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps{
     feedbackType : FeedbackType
-    onFeedbadckRestartFeedbackRequest: () => void;
+    onFeedbackRestartRequested: () => void;
+    onFeedbackSent: () => void;
 }
 export function FeedbackContentStep({
     feedbackType,
-    onFeedbadckRestartFeedbackRequest
+    onFeedbackRestartRequested,
+    onFeedbackSent
 }: FeedbackContentStepProps) {
     const [screenshot, setScreenshot] = useState <string | null>(null)
     const [comment, setComment] = useState('')
@@ -22,6 +24,7 @@ export function FeedbackContentStep({
             comment,
             screenshot
         )
+        onFeedbackSent();
     }
 
     return (
@@ -30,7 +33,7 @@ export function FeedbackContentStep({
                     <button 
                         type="button" 
                         className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
-                        onClick={onFeedbadckRestartFeedbackRequest}
+                        onClick={onFeedbackRestartRequested}
                     >
                         <ArrowLeft weight="bold" className="w-4 h-4" />
                     </button>
@@ -56,6 +59,7 @@ export function FeedbackContentStep({
                     <button
                         disabled = {comment.length === 0}
                         type = "submit"
+                        onClick={handleSubmitFeedback}
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus-outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
                         Enviar feedback
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -7,6 +7,7 @@ import thoughtImageUrl from '../../assets/thought.svg'
 import ideaImageUrl from '../../assets/idea.svg'
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSucessStep } from "./Steps/FeedbackSucessStep";
 
 
 export const feedbackTypes = {
@@ -38,23 +39,33 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm(){
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false)
 
     function handleRestartFeedback(){
+        setFeedbackSent(false)
         setFeedbackType(null);
+        
     }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            
-            
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            { feedbackSent ? (
+                <FeedbackSucessStep onFeedbackRestartRequested={handleRestartFeedback}/>
             ) : (
-                <FeedbackContentStep 
-                    feedbackType={feedbackType}
-                    onFeedbadckRestartFeedbackRequest={handleRestartFeedback}
-                />
-            )}
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                        ) : (
+                            <FeedbackContentStep 
+                                feedbackType={feedbackType}
+                                onFeedbackRestartRequested={handleRestartFeedback}
+                                onFeedbackSent= {() => setFeedbackSent(true)}
+                             />
+                            )}
+                </>
+            ) }
+            
+
             
             <footer className="text-xa text-neutral-400">
                 Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
